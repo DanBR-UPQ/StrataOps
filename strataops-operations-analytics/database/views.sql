64aa7c40478f8646c651fd1cf2a9aec75b9d8019
Daniel Jr Betancourt Razo <124051505@upq.edu.mx>
2026-04-19 00:49:40 -0600

    feat: add SQL views and extend queries

--- strataops-operations-analytics/database/views.sql
+++ strataops-operations-analytics/database/views.sql
@@ -1,16 +1,8 @@
--- Useful KPIs for our analytics
--- We're assuming one row = one shift for an individual machine
-
-
--- KPI 0: TOTAL UNITS
--- Sum of units produced
-SELECT
-    SUM(units_produced) AS total_output
-FROM production_logs;
--- Returns integer
+-- KPIs saved as views for easier Power BI analytics
 
 -- KPI 1: EFFICIENCY
 -- Percentage of useable (not defective) units
+CREATE VIEW kpi_efficiency AS
 SELECT 
     SUM(units_produced - defective_units)::float / SUM(units_produced) AS efficiency
 FROM production_logs;
@@ -19,22 +11,18 @@ FROM production_logs;
 
 -- KPI 2: DEFECT RATE
 -- Opposite of efficiency: percentage of defective units
+CREATE VIEW kpi_defect_rate AS
 SELECT
     SUM(defective_units)::float / SUM(units_produced) AS defect_rate
 FROM production_logs;
 -- Returns number between 0-1. Eg 5% defect rate -> 0.05
 
 
--- Testing if we got the correct numbers..
-SELECT 
-  SUM(units_produced),
-  SUM(defective_units)
-FROM production_logs;
-
 
 -- KPI 3: DOWNTIME
 -- Percentage of time spent on downtime
 -- Assuming a shift = 8hrs (480mins)
+CREATE VIEW kpi_downtime AS
 SELECT
     SUM(downtime_minutes)::float / (COUNT(*) * 480) AS downtime_percentage
 FROM production_logs;
@@ -43,73 +31,89 @@ FROM production_logs;
 
 -- KPI 4: OUTPUT PER MACHINE
 -- Total units produced per machine
+CREATE VIEW kpi_output_per_machine AS
 SELECT
     machine_id AS machine,
     SUM(units_produced) AS total_output -- output is a reserved word sadly
 FROM production_logs 
-    GROUP BY machine_id ORDER BY total_output DESC;
--- Returns table with machine id and units produced. Eg M2, 33928
+GROUP BY machine_id;
+
 
 
 -- KPI 5: WORST MACHINES
 -- Machines with their defect rate and total output
+CREATE VIEW kpi_worst_machines AS
 SELECT
     machine_id AS machine,
     SUM(units_produced) AS total_output,
     SUM(defective_units)::FLOAT / SUM(units_produced) AS defect_rate
 FROM production_logs
-    GROUP BY machine_id ORDER BY defect_rate DESC;
--- Returns table with machine id, output, and defect rate (number between 0-1). Eg M3, 33835, 0.1056
+GROUP BY machine_id;
+
 
 
 -- KPI 6: SHIFT PERFORMANCE
 -- How performance is affected by shift (day / night)
+CREATE VIEW kpi_shift_performance AS
 SELECT
     shift,
     SUM(units_produced) AS total_output,
     SUM(defective_units)::float / SUM(units_produced) AS defect_rate
 FROM production_logs
-    GROUP BY shift ORDER BY defect_rate DESC;
--- Returns table with shift, output, and defect rate (number between 0-1). Eg Night, 84697, 0.065
+GROUP BY shift;
 
 
 -- KPI 7: DAILY TREND
 -- Units produced per day
+CREATE VIEW kpi_daily_trend AS
 SELECT 
   date,
   SUM(units_produced) AS total_output
 FROM production_logs
-    GROUP BY date ORDER BY date;
--- Returns table with date and units produced. Eg 2025-01-01, 1389
+GROUP BY date;
 
 
 -- KPI 8: DOWNTIME PER MACHINE
 -- Machines with their respective downtime
+CREATE VIEW kpi_downtime_per_machine AS
 SELECT
     machine_id AS machine,
     SUM(downtime_minutes)::float / (COUNT(*) * 480) AS downtime_percentage
 FROM production_logs
-    GROUP BY machine_id ORDER BY downtime_percentage DESC;
--- Returns table with machine id and downtime (number between 0-1). Eg M1, 0.189
+GROUP BY machine_id;
 
 
 -- KPI 9: OUTPUT PER MACHINE TYPE
 -- Machine types with their respective output
+CREATE VIEW kpi_output_per_machine_type AS
 SELECT
     m.machine_type AS machine_type,
     SUM(units_produced) AS total_output
-FROM production_logs p JOIN machines m
+FROM production_logs p 
+JOIN machines m
     ON p.machine_id = m.machine_id
-    GROUP BY m.machine_type ORDER BY total_output DESC;
+GROUP BY m.machine_type;
 
 
 -- KPI 10: PERFORMANCE PER MACHINE AGE
 -- Machine installation date with their respective performance
+CREATE VIEW kpi_performance_per_machine_age AS
 SELECT
     m.installation_year,
     SUM(p.units_produced) AS total_output,
     SUM(p.defective_units)::float / SUM(p.units_produced) AS defect_rate
-FROM production_logs p JOIN machines m 
+FROM production_logs p 
+JOIN machines m 
     ON p.machine_id = m.machine_id 
-    GROUP BY m.installation_year ORDER BY m.installation_year DESC;
--- Returns table with machine installation date, output, defect rate (num between 0-1). Eg 2022, 33784, 0.023768647880653564
+GROUP BY m.installation_year;
+-- Returns table with machine installation date, output, defect rate (num between 0-1)
+
+
+-- MASTER KPI
+CREATE VIEW factory_kpis AS
+SELECT
+    SUM(units_produced) AS total_output,
+    SUM(units_produced - defective_units)::float / SUM(units_produced) AS efficiency,
+    SUM(defective_units)::float / SUM(units_produced) AS defect_rate,
+    SUM(downtime_minutes)::float / (COUNT(*) * 480) AS downtime_percentage
+FROM production_logs;
